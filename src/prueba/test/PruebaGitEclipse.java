c13cc97362dec32e656f363c7eb9179322310d0b
montsemsanz <montsemsanz@gmail.com>
2021-03-13 11:39:43 +0100

    Añadido comentario a la clase PruebaGitEclipse

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -2,12 +2,18 @@ package prueba.test;
 
 import prueba.modelo.Saludo;
 
+/**
+ * Punto de entrada a la aplicación
+ * @author Montse
+ *
+ */
 public class PruebaGitEclipse {
 
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan"));
 		System.out.println(saludo.saludar("Isabel"));
+		
 		
 	}
 
